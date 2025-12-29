3d568b5749b6b505e643fd82a53bc11166bdbbbe
Vishal M <mauryavishal0080@gmail.com>
2025-12-29 21:30:16 +0530

    build: output to docs for github pages

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,28 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: "/New_my_Portfolio/", // 👈 repo name same rakho
-  plugins: [
-    react({
-      babel: {
-        plugins: [["babel-plugin-react-compiler"]],
-      },
-    }),
-  ],
+  base: "/New_my_Portfolio/",
+  plugins: [react()],
+  build: {
+    outDir: "docs",
+  },
 });
+
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react";
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   base: "/New_my_Portfolio/", // 👈 repo name same rakho
+//   plugins: [
+//     react({
+//       babel: {
+//         plugins: [["babel-plugin-react-compiler"]],
+//       },
+//     }),
+//   ],
+// });
 
 // import { defineConfig } from "vite";
 // import react from "@vitejs/plugin-react";
